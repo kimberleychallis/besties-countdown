da364134c55940e587bd5a46de2f2459964a18e0
Kimberley Challis <kimchallis@gmail.com>
2021-04-08 18:18:12 +0100

    Created formatTimeLeft function

--- countdown.js
+++ countdown.js
@@ -1,13 +1,38 @@
+// Start with an initial value of 20 seconds
+const timeLimit = 20;
+
+// Initially, no time has passed, but this will count up
+// and subtract from the timeLimit
+let timePassed = 0;
+let timeLeft = timeLimit;
+
+formatTimeLeft = (time) => {
+  // The largest round integer less than or equal to the result of time divided being by 60.
+  const minutes = Math.floor(time / 60);
+
+  // Seconds are the remainder of the time divided by 60 (modulus operator)
+  let seconds = time % 60;
+
+  // If the value of seconds is less than 10, then display seconds with a leading zero
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  // The output in MM:SS format
+  return `${minutes}:${seconds}`;
+};
+
 document.getElementById("app").innerHTML = `
 <div class="container">
+
 <div class="base-timer">
   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <g class="base-timer__circle">
       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
     </g>
   </svg>
-  <span>
-    <!-- Remaining time label -->
+  <span id="base-timer-label" class="base-timer__label">
+    ${formatTimeLeft(timeLeft)}
   </span>
 </div>  
 </div>
